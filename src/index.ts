0982d939346dce89fda4a2bee8189279da6fa9fd
Jakob Helgesson <jakob.helgesson@gmail.com>
2023-02-05 23:24:18 +0000

    Add forgotten module type

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { ENS } from '@ensdomains/ensjs'
-import { ethers } from 'ethers'
+import { ENS } from "@ensdomains/ensjs";
+import { ethers } from "ethers";
 
-const provider = new ethers.providers.JsonRpcProvider(providerUrl)
+const provider = new ethers.providers.JsonRpcProvider(providerUrl);
 
-const ENSInstance = new ENS()
-await ENSInstance.setProvider(provider)
+const ENSInstance = new ENS();
+await ENSInstance.setProvider(provider);
